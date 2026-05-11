c98dba8cd8166f3fc8e7289d05f1e890638d9a0f
Chris Wilker <chriswilker@gmail.com>
2026-05-10 21:08:53 -0700

    Adding new s3 bucket tags to main.tf

--- main.tf
+++ main.tf
@@ -21,11 +21,13 @@ resource "aws_s3_bucket" "orbit_storage" {
   bucket_prefix = "orbit-storage-"
 
   tags = {
-    name      = "Orbit Labs Storage"
-    managedBy = "Spacelift"
-    mission   = "First Launch"
-    project   = "Orbit-labs"
+    name        = "Orbit Labs Storage"
+    managedBy   = "Spacelift"
+    mission     = "First Launch"
+    project     = "Orbit-labs"
+    environment = "demo"
   }
+
 }
 
 output "aws_account_id" {
